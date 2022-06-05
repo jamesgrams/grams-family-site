536d4676edb2e216b074424f298c956175a8e288
James Grams <james@game103.net>
2022-06-05 14:45:31 -0400

    BF

--- theme/header.php
+++ theme/header.php
@@ -2,17 +2,17 @@
 <html>
     <head>
         <title><?php echo $args['title']; ?></title>
-        <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
+        <link rel="stylesheet" type="text/css" href="/wp-content/themes/theme/style.css">
         <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <meta name="description" content="The Grams family website.">
         <meta charset="UTF-8">
-        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri()?>/images/icons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri()?>/images/icons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri()?>/images/icons/favicon-16x16.png">
-        <link rel="manifest" href="<?php echo get_template_directory_uri()?>/images/icons/site.webmanifest">
-        <link rel="mask-icon" href="<?php echo get_template_directory_uri()?>/images/icons/safari-pinned-tab.svg" color="#00381c">
+        <link rel="apple-touch-icon" sizes="120x120" href="/wp-content/themes/theme/images/icons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/theme/images/icons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/theme/images/icons/favicon-16x16.png">
+        <link rel="manifest" href="/wp-content/themes/theme/images/icons/site.webmanifest">
+        <link rel="mask-icon" href="/wp-content/themes/theme/images/icons/safari-pinned-tab.svg" color="#00381c">
         <meta name="msapplication-TileColor" content="#00381c">
         <meta name="theme-color" content="#00381c">
         <?php wp_head()?>
