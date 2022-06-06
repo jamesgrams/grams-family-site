209c34c6aaf0a1c56b10dc1181004b6af2d4d797
James Grams <james@game103.net>
2022-06-06 16:12:36 -0400

    Bug fixes

--- theme/comments.php
+++ theme/comments.php
@@ -3,7 +3,7 @@
     <?php if ( have_comments() ) : ?>
         <h2 class="comments-title">
             <?php
-                printf( _nx( 'One comment on "%2$s"', '%1$s comments on "%2$s"', get_comments_number() ),
+                printf( _nx( 'One comment on "%2$s"', '%1$s comments on "%2$s"', get_comments_number(), 'comments title' ),
                     number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
             ?>
         </h2>
