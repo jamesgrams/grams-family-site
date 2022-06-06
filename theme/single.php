fcd52e51b70eea848260104691143942256d4deb
James Grams <james@game103.net>
2022-06-06 15:37:54 -0400

    Comments

--- theme/single.php
+++ theme/single.php
@@ -1,3 +1,4 @@
 <?php get_header( null, array( "title" => get_the_title() ) ) ?>
 <?php get_template_part( "multi", null, array( "h" => "h2" ) ) ?>
+<?php comments_template() ?>
 <?php get_footer() ?>
